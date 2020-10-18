d5ea845a24fa4bfadb509c6c21e280e8b1f1b1ca
Ignacio Sbaraglia <shogakukan@gmail.com>
2020-10-18 11:37:15 -0300

    Primeros endpoint

--- routes/ingresos.js
+++ routes/ingresos.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Trae todos los ingresos del usuario */
 router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+//Trae un ingreso determinado por ID, debe chequear que sea de ese usuario
+router.get('/:id', async (req, res) =>{
+  res.send('respond with a resource');});
+
+// Agrega un ingreso
+router.post('/', async (req, res) => {
+  res.send('respond with a resource');
+});
+
+// Edita un ingreso
+router.put('/:id', async (req, res) =>{
+  res.send('respond with a resource');
+});
+
+// Elimina un ingreso
+router.delete('/:id', async (req,res) => {
   res.send('respond with a resource');
 });
 
