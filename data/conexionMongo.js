6beb82def5471b81fb51e94f0a0a3a147ac22eee
FerOlivero <fernando.olivero@comafi.com.ar>
2020-11-01 09:33:24 -0300

    Se parametriza la conexion a mongodb. Se cargan las variables en heroku.

--- data/conexionMongo.js
+++ data/conexionMongo.js
@@ -1,8 +1,7 @@
 const chalk = require('chalk');
 const mongoclient = require('mongodb').MongoClient;
-
-//const uriMongodb = 'mongodb+srv://admin:tp2a@cluster0.uak9i.mongodb.net/sample_betp2?retryWrites=true&w=majority';
-const uriMongodb = "mongodb+srv://finanzapp:yCa52mC3AeTTRuod@cluster0.09ljz.mongodb.net/sample_betp2?retryWrites=true&w=majority";
+const dotenv = require('dotenv').config();
+const uriMongodb = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOMAIN}/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`
 const client = new mongoclient(uriMongodb, {useUnifiedTopology: true, useNewUrlParser: true});
 
 async function getConnection(){
