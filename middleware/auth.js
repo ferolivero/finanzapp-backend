e68c49ec53710d296a72ee979eb4b1cb17622742
FerOlivero <fernando.olivero@comafi.com.ar>
2020-11-04 20:05:55 -0300

    Se sube la validacion de google al backend.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const dataUser = require('../data/user');
 const dotenv = require('dotenv').config();
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(['965440611676-l44ls589vsp17lf9nnok70rgbmcndjhn.apps.googleusercontent.com', '965440611676-st0f6b7mpuc25k6afr2ha1eq8k6oabf0.apps.googleusercontent.com']);
 
 async function auth(req, res, next){
     try {
@@ -8,8 +10,8 @@ async function auth(req, res, next){
             throw new Error('Acceso denegado')
         const token = req.header('Authorization').replace('Bearer ','');
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        const user = await dataUser.getUsuario(decoded._id);
-        req.headers['user'] = user._id;
+        //const user = await dataUser.getUsuario(decoded._id);
+        //req.headers['user'] = user._id;
         next();
     } catch (e) {
         res.status(401).send({error: e.message});
@@ -17,7 +19,7 @@ async function auth(req, res, next){
 }
 
 async function generateTokenAuth(user){
-    const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {expiresIn: '1h'});
+    const token = jwt.sign(user, process.env.JWT_SECRET_KEY); //, {expiresIn: '1h'}
     return token;
 }
 
@@ -25,4 +27,24 @@ function getUserFromRequest(req) {
     return req.headers['user'];
 }
 
-module.exports = {auth, generateTokenAuth, getUserFromRequest};
+async function verify(token) {
+    console.log({token});
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: ['965440611676-l44ls589vsp17lf9nnok70rgbmcndjhn.apps.googleusercontent.com', '965440611676-st0f6b7mpuc25k6afr2ha1eq8k6oabf0.apps.googleusercontent.com'],  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload();
+    console.log({payload});
+    const userid = payload['sub'];
+
+    console.log({userid});
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+
+    return payload;
+}
+
+module.exports = {auth, generateTokenAuth, getUserFromRequest, verify};
--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,19 @@ const user = require('../data/user');
 const authMiddleware = require('./../middleware/auth')
 
 router.get('/token', async function(req, res, next) {
-  // Aca deberia realizar la validacion del login
-  console.log(req.headers);
-  const { email, name, photourl } = req.headers;
-  console.log('email', email)
-  if (email){
-    let usuario = await user.getUsuario(email.toString());
+  const { idtoken } = req.headers;
+  const payload = await authMiddleware.verify(idtoken);
+  console.log('email', payload.email)
+  if (payload.email){
+    const usuario = await user.getUsuario(payload.email.toString());
     console.log(usuario);
     if (!usuario) {
       console.log('Creo el usuario en la base de datos')
-      usuario = {_id: email, nombre: name, foto: photourl};
+      usuario = {_id: payload.email, nombre: payload.name, foto: payload.picture, moneda: "$"};
       await user.pushUsuario(usuario);
     }
   
     const token = await authMiddleware.generateTokenAuth(usuario);
-    //   jwt.create(process.env.JWT_SECRET_KEY);
-    // token.setExpiration(new Date().getTime() + 15*60*1000)
     console.log({token})
     res.send(token)
   } else {
