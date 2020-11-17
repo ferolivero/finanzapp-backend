b7ee0298dc19d36d2518915ba2a81491b7304179
FerOlivero <fernando.olivero@comafi.com.ar>
2020-11-17 19:19:59 -0300

    Se agrega el getMovimiento.

--- data/movimiento.js
+++ data/movimiento.js
@@ -1,27 +1,31 @@
-const abm = require('./abm');
+const abm = require('./abm')
 
-const myCollection = 'movimientos';
+const myCollection = 'movimientos'
 
-async function getAllMovimientosDesc(filter = {}){
-    let movimientos = await abm.getCollection(myCollection, filter);
-    return movimientos.sort(compareDates);
+async function getAllMovimientosDesc(filter = {}) {
+  let movimientos = await abm.getCollection(myCollection, filter)
+  return movimientos.sort(compareDates)
 }
 
 async function imputarRecurrentes(movs) {
-    const connectionmongo = await connection.getConnection()
-    const options = { ordered: true };
-    const result = await connectionmongo
-      .db(process.env.MONGODB_DB_NAME)
-      .collection(myCollection)
-      .insertMany(movs, options)
-    return result
-  }
-
-
-function compareDates(a, b) {
-    if (a.fecha > b.fecha) return -1;
-    if (b.fecha > a.fecha) return 1;
-    return 0;
+  const connectionmongo = await connection.getConnection()
+  const options = { ordered: true }
+  const result = await connectionmongo
+    .db(process.env.MONGODB_DB_NAME)
+    .collection(myCollection)
+    .insertMany(movs, options)
+  return result
 }
 
-module.exports = {getAllMovimientosDesc, imputarRecurrentes}
+async function getMovimiento(filter = {}) {
+  // filter.tipo = myType
+  return await abm.getItem(myCollection, filter)
+}
+
+function compareDates(a, b) {
+  if (a.fecha > b.fecha) return -1
+  if (b.fecha > a.fecha) return 1
+  return 0
+}
+
+module.exports = { getAllMovimientosDesc, imputarRecurrentes, getMovimiento }
