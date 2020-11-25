ed52dd054b36031a9934c117657db9929938cd09
shogakukan <62673508+shogakukan@users.noreply.github.com>
2020-11-24 21:23:04 -0300

    Update configs.js

--- routes/configs.js
+++ routes/configs.js
@@ -3,11 +3,12 @@ const authMiddleware = require('../middleware/auth');
 var router = express.Router();
 const userData = require('../data/user')
 
-/* Trae todos los datos del usuario */
-router.get('/', authMiddleware.auth, async (req, res) => {
-  const user = authMiddleware.getUserFromRequest(req);
 
-  const result = await userData.getUsuarioDatos(req.db, { _id: user })
+router.get('/user', authMiddleware.auth, async (req, res) => {
+  const user = authMiddleware.getUserFromRequest(req)
+  const result = await userData.getUsuario(req.db, {
+    id: user,
+  })
   res.json(result)
 })
 
